c945eab0f187fbb55d6bbfa2cdb2c0c27b79cc5b
Pawan Bahuguna <40817441+pawanbahuguna@users.noreply.github.com>
2024-10-19 14:49:56 -0400

    Remove colour
    Removing color as breaking some logging.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,8 @@
 # Script to Download job-level logs for the past 24hrs or all the logs
 # Author: Pawan Bahuguna
 # GitHub: https://github.com/pawanbahuguna/action-logs
-# Version: v1.0
+# Version: v2.0
 ########################
-
-C_RED="\033[0;31m" # Red
-C_GRN="\033[0;32m" # Green
-C_BLU="\033[0;34m" # Blue
-C_RST="\033[0m"    # Reset
 
 JOB_FILE=jobID.txt
 
@@ -36,7 +31,7 @@ else
   if [ "$(uname)" = "Linux" ]; then
     if [ -f /etc/alpine-release ]; then
       # Alpine Linux
-      OLD_DATE=$(date -u -D '%s' -d $(( $(date +%s) - 24*60*60 )) +'%Y-%m-%dT%H:%M:%SZ')
+      OLD_DATE=$(date -u -R | awk '{ print strftime("%Y-%m-%dT%H:%M:%SZ", systime() - 24*60*60) }')
     else
       # Other Linux
       OLD_DATE=$(date -d '24 hours ago' +'%Y-%m-%dT%H:%M:%SZ')
@@ -48,27 +43,16 @@ else
   echo "Will only process 24hrs logs before or equal to $OLD_DATE"
 fi
 
-error_msg () {
- printf "${C_RED}$*${C_RST}"
-}
-
-success_msg () {
- printf "${C_GRN}$*${C_RST}"
-}
-
-info_msg () {
- printf "${C_BLU}$*${C_RST}"
-}
 
 clean_up () {
-  info_msg "Starting cleanup"
+  echo "Starting cleanup"
   rm -f $JOB_FILE
-  success_msg "cleanup Completed!!"
+  echo "cleanup Completed!!"
 }
 
 # Get Workflow run
 get_run_id () {
-  info_msg "Searching Workflow Runs"
+  echo "Searching Workflow Runs"
   RUNS=$(curl -s -H "Authorization: token $GH_TOKEN" "https://api.github.com/repos/$GH_REPO/actions/runs?per_page=100")
   RUN_ID=$(echo "$RUNS" | jq -r --arg OLD_DATE "$OLD_DATE" '.workflow_runs[] | select(.created_at >= $OLD_DATE) | .id' )
   TOTAL_COUNT=$(echo "$RUNS" | jq -r '.total_count')
@@ -76,7 +60,7 @@ get_run_id () {
 
 # Get jobs from the Workflow
 get_job_id () {
-  info_msg "Fetching Job IDs"
+  echo "Fetching Job IDs"
   for run_id in $RUN_ID; do
    curl -s -L -H "Authorization: Bearer $GH_TOKEN" "https://api.github.com/repos/$GH_REPO/actions/runs/$run_id/jobs" | jq -r '.jobs[].id' >> $JOB_FILE
   done 
@@ -87,7 +71,7 @@ get_jobs_logs () {
   mkdir "$LOGS_DIR"
   cat $JOB_FILE | while IFS= read -r JOBID
   do
-   info_msg "Writing logs for job $JOBID"
+   echo "Writing logs for job $JOBID"
    JOB_LOGS="logs-$JOBID-$(date +'%Y-%m-%d').txt"
    curl -s -L -H "Authorization: Bearer $GH_TOKEN" "https://api.github.com/repos/$GH_REPO/actions/jobs/$JOBID/logs" -o "$LOGS_DIR/$JOB_LOGS"
   done
@@ -96,17 +80,17 @@ get_jobs_logs () {
 get_run_id
 # Check if there are any workflow runs
 if [ "$TOTAL_COUNT" -eq 0 ]; then
-    error_msg "No workflow runs found."
+    echo "No workflow runs found."
     exit 1
 else
     get_job_id
     if [ -f $JOB_FILE ]; then
      get_jobs_logs
-     success_msg "Logs Downloaded and saved!"
+     echo "Logs Downloaded and saved!"
      clean_up
      exit 0
     else 
-     error_msg "Job ID file not found or no jobs to process, exiting."
+     echo "Job ID file not found or no jobs to process, exiting."
      exit 1
     fi
 fi
